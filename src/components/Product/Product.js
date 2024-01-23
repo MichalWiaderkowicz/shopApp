0a5f6ed5c752a9c2000a5e4c755c17bf0ef746ef
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-01-23 21:34:29 +0100

    useMemo Price

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,6 +1,6 @@
 import styles from './Product.module.scss';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import ProductImage from '../ProductImage/ProductImage.js';
 import ProductForm from '../ProductForm/ProductForm.js';
 
@@ -11,20 +11,20 @@ const {title, basePrice, colors, name, sizes, } = props
 const [currentColor, setCurrentColor] = useState(colors[0]);
 const [currentSize, setCurrentSize] = useState(sizes[0].name);
 
-  const getPrice = () => {
+  const price = useMemo(() => {
     let sizePrice = sizes.find((element) => element.name === currentSize);
     let addPrice = sizePrice.additionalPrice;
-    let totalPrice = basePrice + addPrice;
+    const totalPrice = basePrice + addPrice;
     
     return totalPrice;
-  };
+  },[basePrice, sizes, currentSize]);
 
   const cart = e => {
     e.preventDefault();
     console.log('Summary',);
     console.log('==========',);
     console.log('Name', title);
-    console.log('Price', getPrice());
+    console.log('Price', price());
     console.log('Size', currentSize);
     console.log('Color', currentColor);
   }
@@ -35,7 +35,7 @@ const [currentSize, setCurrentSize] = useState(sizes[0].name);
       <div>
         <header>
           <h2 className={styles.name}>{title}</h2>
-          <span className={styles.price}>Price: {getPrice()}$</span>
+          <span className={styles.price}>Price: {price}$</span>
         </header>
         <ProductForm cart={cart} colors={colors} sizes={sizes} currentColor={currentColor} currentSize={currentSize} setCurrentColor={setCurrentColor} setCurrentSize={setCurrentSize} />
       </div>
